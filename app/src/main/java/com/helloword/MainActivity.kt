6fc714cfe3351e4c22fa76509622ebf91e9da5b1
bhanubispl <bhanu.bispl@gmail.com>
2023-10-19 10:44:05 +0530

    Hello world

--- app/src/main/java/com/helloword/MainActivity.kt
+++ app/src/main/java/com/helloword/MainActivity.kt
@@ -1,33 +1,21 @@
 package com.helloword
 
 import android.os.Bundle
-import android.print.PrintAttributes.Margins
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
+
 import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.tooling.preview.PreviewParameter
-import androidx.compose.ui.unit.dp
-import com.helloword.ui.theme.HellowordTheme
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-
-            Text("Hello Jetpack Compose")
+            Text("Hello Welcome jetpack compose")
+            Text("I am Exciting  Welcome jetpack compose")
+        }
     }
-}}
+}
 
 
 /*Jetpack Compose is built around composable functions.
